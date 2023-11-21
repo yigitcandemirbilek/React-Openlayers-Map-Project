6aa70539f3e5da903de1ffe601beeb810fd36c56
yigitcandemirbilek <yigitcandemirbilek@hotmail.com>
2023-11-21 15:04:56 +0300

    Line tool added put space between toolbar buttons

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -12,7 +12,8 @@ import { fromLonLat } from 'ol/proj';
 const MapComponent = () => {
     const turkeyCenter = fromLonLat([35.1683, 37.1616]);
     const [map, setMap] = useState(null);
-    let drawInteraction;
+    let drawPolygonInteraction;
+    let drawLineInteraction;
 
 
   useEffect(() => {
@@ -44,18 +45,19 @@ const MapComponent = () => {
     };
   }, []);
 
-  const activateDrawTool = () => {
-    drawInteraction = new Draw({
+  const activatePolygonDrawTool = () => {
+    deactivateDrawTools();
+    drawPolygonInteraction = new Draw({
       source: map.getLayers().item(1).getSource(),
       type: 'Polygon',
     });
 
-    drawInteraction.on('drawend', (event) => {
+    drawPolygonInteraction.on('drawend', (event) => {
       const coordinates = event.feature.getGeometry().getCoordinates();
       console.log('Polygon Koordinatları:', coordinates);
     });
 
-    map.addInteraction(drawInteraction);
+    map.addInteraction(drawPolygonInteraction);
 
     const modifyInteraction = new Modify({ source: map.getLayers().item(1).getSource() });
     map.addInteraction(modifyInteraction);
@@ -64,14 +66,42 @@ const MapComponent = () => {
     map.addInteraction(snapInteraction);
   };
 
-  const deactivateDrawTool = () => {
-    if (drawInteraction) {
-      map.removeInteraction(drawInteraction);
+  const activateLineDrawTool = () => {
+    deactivateDrawTools();
+    drawLineInteraction = new Draw({
+      source: map.getLayers().item(1).getSource(),
+      type: 'LineString',
+    });
+
+    drawLineInteraction.on('drawend', (event) => {
+      const coordinates = event.feature.getGeometry().getCoordinates();
+      console.log('Line Koordinatları:', coordinates);
+    });
+
+    map.addInteraction(drawLineInteraction);
+
+    const modifyInteraction = new Modify({ source: map.getLayers().item(1).getSource() });
+    map.addInteraction(modifyInteraction);
+
+    const snapInteraction = new Snap({ source: map.getLayers().item(1).getSource() });
+    map.addInteraction(snapInteraction);
+  };
+
+  const deactivateDrawTools = () => {
+    if (drawPolygonInteraction) {
+      map.removeInteraction(drawPolygonInteraction);
+    }
+    if (drawLineInteraction) {
+      map.removeInteraction(drawLineInteraction);
     }
   };
 
-  const handleDrawButtonClick = () => {
-    activateDrawTool();
+  const handleDrawButtonClick  = () => {
+    activatePolygonDrawTool();
+  };
+
+  const handleLineDrawButtonClick = () => {
+    activateLineDrawTool();
   };
 
   return (
@@ -81,6 +111,10 @@ const MapComponent = () => {
 <path opacity="0.25" d="M0 24V3.7983L9.98533 10.7806L24 0V24H0Z" fill="white"/>
 <path d="M10.1544 10.08L0 3.36V24H24V0L10.1544 10.08ZM22.8 22.8H1.2V5.5932L10.1868 11.5404L22.8 2.358V22.8Z" fill="white"/>
 </svg></button>
+<button onClick={handleLineDrawButtonClick} className='linebtn'><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M21 6H21.046L15.796 15H14.852L10 9.455V7H7V9.926L1.862 18H0V21H3V18.074L8.138 10H9.148L14 15.545V18H17V15H16.954L22.204 6H24V3H21V6ZM8 8H9V9H8V8ZM2 20H1V19H2V20ZM16 17H15V16H16V17ZM23 4V5H22V4H23Z" fill="white"/>
+</svg></button>
+
       </div>
     </div>
   );

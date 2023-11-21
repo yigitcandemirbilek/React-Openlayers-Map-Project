d46d88551b3a27bf9f1adefe21bc8decd3b6a975
yigitcandemirbilek <yigitcandemirbilek@hotmail.com>
2023-11-21 14:53:07 +0300

    Polygon tool was bound to a button event

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -4,7 +4,7 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
-import { Draw, Modify, Snap } from 'ol/interaction';
+import { Draw, Modify, Pointer, Snap } from 'ol/interaction';
 import { Vector as VectorLayer } from 'ol/layer';
 import { Vector as VectorSource } from 'ol/source';
 import { fromLonLat } from 'ol/proj';
@@ -12,9 +12,11 @@ import { fromLonLat } from 'ol/proj';
 const MapComponent = () => {
     const turkeyCenter = fromLonLat([35.1683, 37.1616]);
     const [map, setMap] = useState(null);
+    let drawInteraction;
+
 
   useEffect(() => {
-    const map = new Map({
+    const initialMap  = new Map({
       target: 'map',
       layers: [
         new TileLayer({
@@ -33,10 +35,18 @@ const MapComponent = () => {
       source: vectorSource,
     });
 
-    map.addLayer(vectorLayer);
+    initialMap.addLayer(vectorLayer);
 
-    const drawInteraction = new Draw({
-      source: vectorSource,
+    setMap(initialMap);
+
+    return () => {
+      initialMap.setTarget(null);
+    };
+  }, []);
+
+  const activateDrawTool = () => {
+    drawInteraction = new Draw({
+      source: map.getLayers().item(1).getSource(),
       type: 'Polygon',
     });
 
@@ -47,21 +57,31 @@ const MapComponent = () => {
 
     map.addInteraction(drawInteraction);
 
-    const modifyInteraction = new Modify({ source: vectorSource });
+    const modifyInteraction = new Modify({ source: map.getLayers().item(1).getSource() });
     map.addInteraction(modifyInteraction);
 
-    const snapInteraction = new Snap({ source: vectorSource });
+    const snapInteraction = new Snap({ source: map.getLayers().item(1).getSource() });
     map.addInteraction(snapInteraction);
+  };
 
-    setMap(map);
+  const deactivateDrawTool = () => {
+    if (drawInteraction) {
+      map.removeInteraction(drawInteraction);
+    }
+  };
 
-    return () => {
-      map.setTarget(null);
-    };
-  }, []);
+  const handleDrawButtonClick = () => {
+    activateDrawTool();
+  };
 
   return (
     <div id="map" style={{ width: '100%', height: '1000px' }}>
+      <div className='toolbar'>
+      <button className='polygonbtn' onClick={handleDrawButtonClick}><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path opacity="0.25" d="M0 24V3.7983L9.98533 10.7806L24 0V24H0Z" fill="white"/>
+<path d="M10.1544 10.08L0 3.36V24H24V0L10.1544 10.08ZM22.8 22.8H1.2V5.5932L10.1868 11.5404L22.8 2.358V22.8Z" fill="white"/>
+</svg></button>
+      </div>
     </div>
   );
 };

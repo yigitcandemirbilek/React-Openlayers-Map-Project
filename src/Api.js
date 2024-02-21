6c6c562bcbdf8403969e96e33a1231939c533bba
yigitcandemirbilek <yigitcandemirbilek@hotmail.com>
2024-02-21 18:41:37 +0300

    Also added drawing types sent to api

--- src/Api.js
+++ src/Api.js
@@ -1,19 +1,26 @@
-// axios ve ol kütüphanelerini içe aktar
 import axios from 'axios';
 import WKT from 'ol/format/WKT';
 import Point from 'ol/geom/Point';
 import Polygon from 'ol/geom/Polygon';
 import LineString from 'ol/geom/LineString';
 
-
 // PostgreSQL tablosuna koordinatları kaydetmek için bir işlev
 export const saveCoordinatesToPostgres = async (coordinatesArray) => {
   try {
     // Axios ile koordinatları PostgreSQL'e tek tek kaydet
     const responses = await Promise.all(coordinatesArray.map(async (coordinates) => {
-      // Koordinatları WKT formatına dönüştür
-      const wktFormat = new WKT();
-      const wktGeometry = wktFormat.writeGeometry(new Point(coordinates));
+      let wktGeometry;
+      // Koordinat türüne göre WKT geometrisini oluştur
+      if (coordinates.length === 2) {
+        // Nokta
+        wktGeometry = new WKT().writeGeometry(new Point(coordinates));
+      } else if (coordinates[0].length === 2) {
+        // Çizgi
+        wktGeometry = new WKT().writeGeometry(new LineString(coordinates));
+      } else {
+        // Poligon
+        wktGeometry = new WKT().writeGeometry(new Polygon([coordinates]));
+      }
       
       // Axios ile POST isteği yaparak koordinatları PostgreSQL'e kaydet
       return axios.post('https://localhost:7196/api/SpatialData', { wkt: wktGeometry });
